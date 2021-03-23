ffc27f010dd5368cbf9a88f4d4d3bd31069c1f3b
liujang <41243924+liujang@users.noreply.github.com>
2021-03-23 18:11:52 +0800

    Update bqb.sh

--- bqb.sh
+++ bqb.sh
@@ -115,6 +115,3 @@ echo -e "是否为直连节点上ws加密:
   echo "已为节点增加ws加密"
   else
   wget https://github.com/Ehco1996/ehco/releases/download/v1.0.3/ehco_1.0.3_linux_amd64 && mv ehco_1.0.3_linux_amd64 ehco && chmod +x ehco
-  echo "脚本3秒后停止"
-  sleep 3
-  exit
