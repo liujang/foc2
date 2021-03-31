d0e78dce59070226f8f5894941d416a98430f301
liujang <41243924+liujang@users.noreply.github.com>
2021-03-31 22:47:14 +0800

    Create fgcloud.sh

---
+++ fgcloud.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+sleep 3
+if [[ "$EUID" -ne 0 ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+    
+     if [[ $release = "ubuntu" || $release = "debian" ]]; then
+    PM='apt'
+  elif [[ $release = "centos" ]]; then
+    PM='yum'
+  else
+    exit 1
+  fi
+  # PM='apt'
+  if [ $PM = 'apt' ] ; then
+    apt-get update -y
+    apt-get install vim curl git wget zip unzip python3 python3-pip git -y
+elif [ $PM = 'yum' ]; then
+    yum update -y
+    yum install vim curl git wget zip unzip python3 python3-pip git -y
+fi
+pip3 install --upgrade pip
+echo -e
+cd
+echo -e
+git clone https://github.com/zhj18/fgcloud.git
+echo -e
+cd fgcloud
+pip3 install -r requirements.txt
+sleep 3
+cp apiconfig.py userapiconfig.py && cp config.json user-config.json
+echo -e "
+ ${GREEN} 1.web
+ ${GREEN} 2.db
+ "
+ read -p "您输入你要的对接方式:" aNum
+if [ "$aNum" = "1" ];then
+read -p "请输入网站域名(末尾不要有/,列如www.baidu.com):" webapi1
+sleep 1
+sed -i '16s/123456/'${webapi1}'/' userapiconfig.py
+read -p "请输入网站mukey:" key
+ echo "网站mukey为：${key}"
+ sleep 1
+ sed -i '17s/123/'${key}'/' userapiconfig.py
+ read -p "请输入节点序号:" node
+ echo "节点序号为：${node}"
+ sleep 1
+ sed -i '2s/0/'${node}'/' userapiconfig.py
+ elif [ "$aNum" = "2" ] ;then
+ sed -i '14s/modwebapi/glzjinmod/' userapiconfig.py
+ read -p "请输入数据库地址:" ip
+ echo "数据库地址为：${ip}"
+ sleep 1
+ sed -i '23s/127.0.0.1/'${ip}'/' userapiconfig.py
+ read -p "请输入数据库用户名:" user
+ echo "数据库用户名为：${user}"
+ sleep 1
+ sed -i '25s/ss/'${user}'/' userapiconfig.py
+ read -p "请输入数据库名:" db
+ echo "数据库名为：${db}"
+ sleep 1
+ sed -i '27s/shadowsocks/'${db}'/' userapiconfig.py
+ read -p "请输入数据库密码:" passwd
+ echo "数据库密码为：${passwd}"
+ sleep 1
+ sed -i '26s/ss/'${passwd}'/' userapiconfig.py
+ read -p "请输入节点序号:" node
+ echo "节点序号为：${node}"
+ sleep 1
+ sed -i '2s/0/'${node}'/' userapiconfig.py
+  else
+            echo "你他妈是猪吗，就两个数字给你选，你都选错，滚！！！"
+            fi
+ echo "是否为nat对接"
+ echo -e "
+ ${GREEN} 1.是
+ ${GREEN} 2.否
+ "
+ read -p "您输入你要的对接方式:" bNum
+if [ "$bNum" = "1" ];then
+read -p "请输入nat端口:" natport
+sed -i '4s/29382/'${natport}'/' user-config.json
+else
+echo "不做改变"
+            fi
+cd
+cd fgcloud && chmod +x run.sh && ./run.sh
+echo "已经对接完成！！!。"
