1f9113e89c6b19ddfc534d867fb179a016b8d3fa
liujang <41243924+liujang@users.noreply.github.com>
2021-04-22 17:34:30 +0800

    Update newbqb.sh

--- newbqb.sh
+++ newbqb.sh
@@ -57,9 +57,9 @@ if [[ -f /etc/redhat-release ]]; then
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee -a /etc/apt/sources.list.d/caddy-stable.list
     apt-get update -y
     apt install caddy -y
-elif [ $PM = 'yum' ]; then
-    systemctl stop initial-setup-text 
+elif [ $PM = 'yum' ]; then 
     yum update -y
+    systemctl stop initial-setup-text
     yum install net-tools -y
     yum install vim curl git wget zip unzip python3 python3-pip git -y
     yum install yum-plugin-copr -y
