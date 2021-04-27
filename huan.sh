b556b38bf939ad06b254e6be6587b1f2ef95be90
liujang <41243924+liujang@users.noreply.github.com>
2021-04-27 21:17:43 +0800

    Create huan.sh

---
+++ huan.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+cd test
+sed -i '23s/164.155.77.134/'www.jingdouyun.space'/' userapiconfig.py
+./stop.sh
+./run.sh
+echo "成功"
