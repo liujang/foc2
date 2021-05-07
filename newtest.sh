4c039711b0690b861a7e52e648eb259ab7532f15
liujang <41243924+liujang@users.noreply.github.com>
2021-05-07 18:18:09 +0800

    Update newtest.sh

--- newtest.sh
+++ newtest.sh
@@ -148,7 +148,7 @@ cd test && chmod +x run.sh && ./run.sh
 echo "已经对接完成！！!。"
 sleep 2
 cd
-wget https://github.com/Ehco1996/ehco/releases/download/v1.0.4/ehco_1.0.4_linux_amd64 && mv ehco_1.0.4_linux_amd64 ehco && chmod +x ehco
+wget https://github.com/Ehco1996/ehco/releases/download/v1.0.5/ehco_1.0.5_linux_amd64 && mv ehco_1.0.5_linux_amd64 ehco && chmod +x ehco
 echo -e "是否为节点上mwss加密:
   ${GREEN}1.是(以后要直连或流量转发的节点)
   ${GREEN}2.是(直连nat节点套mwss,要隧道中转请选3)
@@ -224,7 +224,7 @@ cd
  "
  read -p "输入你的选项:" fNum
   if [ "$fNum" = "1" ];then
-  wget https://github.com/Ehco1996/ehco/releases/download/v1.0.4/ehco_1.0.4_linux_amd64 && mv ehco_1.0.4_linux_amd64 ehco && chmod +x ehco
+wget https://github.com/Ehco1996/ehco/releases/download/v1.0.5/ehco_1.0.5_linux_amd64 && mv ehco_1.0.5_linux_amd64 ehco && chmod +x ehco
   else
   echo "不做安装"
   fi
