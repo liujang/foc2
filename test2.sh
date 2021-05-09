4998e5fe3a19944321557d23094dafd49dd37a94
liujang <41243924+liujang@users.noreply.github.com>
2021-05-09 18:05:52 +0800

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -84,6 +84,9 @@ echo -e "
  "
  read -p "您输入你要的对接方式:" aNum
 if [ "$aNum" = "1" ];then
+read -p "请输入网站域名(末尾不要有/,列如www.baidu.com):" webapi1
+sleep 1
+sed -i '16s/123456/'${webapi1}'/' userapiconfig.py
 read -p "请输入网站mukey:" key
  echo "网站mukey为：${key}"
  sleep 1
