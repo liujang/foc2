08e7c6ed047c480df7dfbcbf67bb03babefe18d2
liujang <41243924+liujang@users.noreply.github.com>
2021-05-11 10:23:36 +0800

    Create cqtest.sh

---
+++ cqtest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+cd
+cd test
+chmod +x stop.sh && ./stop.sh
+chmod +x run.sh && ./run.sh
