9d6ab25c2f8419518f0b248747e2415727af4dc7
liujang <41243924+liujang@users.noreply.github.com>
2021-05-31 19:21:23 +0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -6,10 +6,12 @@ BLUE="\033[0;36m"
 FUCHSIA="\033[0;35m"
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
-cd && cd /etc/caddy/
-caddy stop
+cd && cd /usr/local/caddy/
+ps -ax | grep caddy
+kill `caddy-id`
 sleep 2
-caddy start
+nohup caddy &
+send "\03" 
 sleep 10
 echo -e
 cd
