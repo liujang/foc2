fd42fc21d0350f42e4e1b8ca17edf8fb0e8e24ee
liujang <41243924+liujang@users.noreply.github.com>
2021-06-11 16:51:27 +0800

    Update newbqb.sh

--- newbqb.sh
+++ newbqb.sh
@@ -329,8 +329,8 @@ echo -e "
  cd bqb- && chmod +x stop.sh && ./stop.sh
  read -p "请输入ssr旧端口:" oldport
  read -p "请输入ssr新端口:" newport
-sed -i '4s/${oldport}/'${newport}'/' user-config.json
-sed -i '22s/${oldport}/'${newport}'/' user-config.json
+sed -i '4s/'${oldport}'/'${newport}'/' user-config.json
+sed -i '22s/'${oldport}'/'${newport}'/' user-config.json
 chmod +x run.sh && ./run.sh
 echo "已更换完成，记得前端网站改端口哦！！！"
 elif [ "$gNum" = "2" ] ;then
