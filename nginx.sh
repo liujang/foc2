0bd132cd8cd4d4fbe6149066df3c008ffe7b0055
liujang <41243924+liujang@users.noreply.github.com>
2021-10-13 13:26:21 +0800

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+cd
+systemctl restart nginx
